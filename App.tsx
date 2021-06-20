0ac2432e5783c0a6dead9ca9431f56216829bf60
Sooyeon Kang <sooyeon.kang@vodafone.com>
2021-06-20 23:36:15 +0100

    fetch weather api

--- App.tsx
+++ App.tsx
@@ -1,24 +1,49 @@
-import 'react-native-gesture-handler';
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { SafeAreaProvider } from 'react-native-safe-area-context';
+import React, { Component, ReactNode } from 'react';
+import { Text, View, SafeAreaView, StyleSheet } from 'react-native';
 
-import useCachedResources from './hooks/useCachedResources';
-import useColorScheme from './hooks/useColorScheme';
-import Navigation from './navigation';
-
-export default function App() {
-  const isLoadingComplete = useCachedResources();
-  const colorScheme = useColorScheme();
-
-  if (!isLoadingComplete) {
-    return null;
-  } else {
-    return (
-      <SafeAreaProvider>
-        <Navigation colorScheme={colorScheme} />
-        <StatusBar />
-      </SafeAreaProvider>
-    );
+export default class App extends React.Component {
+  
+  state = {
+    city: '',
+    description: '',
+    main: ''
   }
+
+  componentDidMount () {
+    fetchApi()
+    .then(res => {
+      this.setState({
+        city: res.name,
+        description: res.weather[0].description,
+        main: res.weather[0].main
+      })
+    })
+  }
+
+  public render() {
+    return (
+      <SafeAreaView>
+        <View style={styles.container}>
+          <Text>{this.state.city}</Text>
+          <Text>{this.state.description}</Text>
+          <Text>{this.state.main}</Text>
+        </View>
+      </SafeAreaView>
+    )
+  }
+}
+
+
+const styles = StyleSheet.create({
+  container: {
+    marginVertical: 10,
+    padding: 10,
+    backgroundColor: 'yellow',
+    color: 'black'
+  }
+})
+
+const fetchApi = async () => {
+  const res = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=London&appid=8eeb256097b74905d07244c6e174b458`)
+  return res.json();
 }
---
+++ components/Hello.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+export default function Hello ():JSX.Element {
+    return (
+        <Text>Hello</Text>
+    )
+}
