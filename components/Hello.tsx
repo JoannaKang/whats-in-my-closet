1b97bd17cd08b9f7cfd463b48e117950ba155a6b
Sooyeon Kang <jfrences11@gmail.com>
2021-06-22 00:00:25 +0100

    email address change

--- components/Hello.tsx
+++ components/Hello.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Text, View } from 'react-native';
 import styled from 'styled-components/native'
+
 interface WeatherInfo {
     city : string;
     description: string;
